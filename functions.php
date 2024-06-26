f59349a74209eea9d13e1d802420f8792194ddf9
Masud Hasan <hasanmasudnet@gmail.com>
2024-06-26 19:21:57 +0600

    Structure

--- functions.php
+++ functions.php
@@ -1,12 +1,4 @@
 <?php
-// Theme setup
-function wprealizer_starter_setup() {
-    // Add theme support
-    add_theme_support( 'title-tag' );  // Enables title tag support
-    add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );  // Enables HTML5 support
-}
-add_action( 'after_setup_theme', 'wprealizer_starter_setup' );
-
 // Custom function to add content after the main content
 function custom_content_after_main() {
     echo '<p>This is custom content added after the main content.</p>';
