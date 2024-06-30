c7a894ffae555efc5591d35424dc96e0af152754
Masud Hasan <hasanmasudnet@gmail.com>
2024-06-30 18:50:06 +0600

    Elementor

--- functions.php
+++ functions.php
@@ -1,10 +1,4 @@
 <?php
-// Custom function to add content after the main content
-function custom_content_after_main() {
-    echo '<p>This is custom content added after the main content.</p>';
-}
-// Attach the custom function to the custom action hook
-add_action('wprealizer_after_main_content', 'custom_content_after_main');
 function marketi_enqueue_scripts() {
     // Favicon
     echo '<link rel="shortcut icon" href="' . get_template_directory_uri() . '/assets/images/favicon.png" />';
--- page.php
+++ page.php
@@ -1,3 +1,13 @@
 <?php get_header(); ?>
-
+<div class="container">
+<?php
+    if ( have_posts() ):
+        while ( have_posts() ): the_post();
+            get_template_part( 'template-parts/content', 'page' );
+        endwhile;
+    else:
+        get_template_part( 'template-parts/content', 'none' );
+    endif;
+?>
+</div>
 <?php get_footer(); ?>
